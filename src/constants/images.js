e1fd3173dfc7877ca296f7e0b40e76651cbca329
William Hortone <willhortone@gmail.com>
2023-05-03 15:53:27 +0800

    add

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,3 +1,21 @@
 import logo from "../assets/logo.png";
+import machine1 from "../assets/machine1.png";
+import machine2 from "../assets/machine2.png";
+import machine3 from "../assets/machine3.png";
+import machine4 from "../assets/machine4.png";
+import machine5 from "../assets/machine5.png";
+import machine6 from "../assets/machine6.png";
+import alters from "../assets/alters.png";
+import casque from "../assets/casque.png";
 
-export default { logo };
+export default {
+  logo,
+  machine1,
+  machine2,
+  machine3,
+  machine4,
+  machine5,
+  machine6,
+  alters,
+  casque,
+};
--- src/containers/presentation/Presentation.js
+++ src/containers/presentation/Presentation.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./presentation.css";
+import { Button } from "../../components";
 
 const Presentation = () => {
-  return <div>Presentation</div>;
+  return (
+    <div className="app__presentation">
+      <div className="app__presentation-left">
+        <h1>FOR MEN</h1>
+        <div className="app__presentation-left-container">
+          <h2>SPRING COLLECTION</h2>
+          <p className="presentation-p p__opensans">
+            Donec ornare, nunc quis cursus hendrerit, tortor turpis sodales
+            turpis, quis sodales est eros id mi. Nam placerat mauris nulla, sed
+            vulputate magna rutrum at. Sed eu augue tempor, porttitor metus
+            eget, convallis odio.
+          </p>
+          <Button />
+        </div>
+      </div>
+      <div className="app__presentation-right"></div>
+    </div>
+  );
 };
 
 export default Presentation;
